72a2a2ced6dea4d76d340fb00c3c6fca8d903459
evanalifian <evanalifian@gmal.com>
2025-12-20 00:19:32 +0700

    use Auth controller

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use SPKPendakian\Controllers\RankingController;
+use SPKPendakian\Controllers\AuthController;
 use SPKPendakian\Core\Router;
 use SPKPendakian\Controllers\HomeController;
 
 Router::get("/", HomeController::class, "index");
 Router::get("/dokumentasi", HomeController::class, "docs");
 Router::get("/dokumentasi/reference_journal.pdf", HomeController::class, "getJournal");
-Router::get("/login", Auth::class, "loginPage");
+Router::get("/login", AuthController::class, "loginPage");
 
 Router::run();
